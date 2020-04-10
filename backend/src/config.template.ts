1729deb52a7eea2b3234fa9851ba7672f9112aa5
Andrew Russell <arustle@pm.me>
2020-04-10 14:23:14 -0400

    chore: update gitignore and config file

---
+++ backend/src/config.template.ts
@@ -0,0 +1,10 @@
+// TODO: Rename this file as config.ts
+
+// TODO: Provide a URL that can be used to download a certificate that can be used
+// to verify JWT token signature.
+// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+// TODO: Provide the Key ID (kid) for the specific key within the JWK Set
+export default {
+  jwksUrl: '...',
+  kid: '...'
+}
--- client/src/config.template.ts
+++ client/src/config.template.ts
@@ -1,3 +1,5 @@
+// TODO: Rename this file as config.ts
+
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
