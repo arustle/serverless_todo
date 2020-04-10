c2c6d943effe3ac4d53a2b9dcfc1881f247031eb
Andrew Russell <arustle@pm.me>
2020-04-10 15:10:14 -0400

    feat: integrate JSON Web Key Set endpoint

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,17 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
+import authConfig from '../../config';
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = authConfig.jwksUrl;
+const kid = authConfig.kid;
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,12 +55,19 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
+
+
+
+
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if (!authHeader) throw new Error('No authentication header!');
+  if (!authHeader.toLowerCase().startsWith('bearer ')) throw new Error('Invalid authentication header!');
+
+
+  const cert = await getCert();
+
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +80,29 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+
+
+interface ISigningKey {
+  keys: [
+    {
+      kid: string,
+      x5c: [string]
+    }
+  ],
+}
+
+function getCert (): Promise<string> {
+   return Axios.get(jwksUrl)
+     .then((res) => {
+       const signingKey: ISigningKey  = res.data;
+       if (signingKey.keys && Array.isArray(signingKey.keys)) {
+         const key = signingKey.keys.find(x => x.kid === kid);
+         if (key) {
+           return key.x5c[0];
+         }
+       }
+       throw new Error("Key not found!");
+     })
 }
